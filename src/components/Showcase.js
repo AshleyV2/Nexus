1a83888ac32d91fe0fc9ca03ebab6afee231b5e9
AshleyV2 <82898109+AshleyV2@users.noreply.github.com>
2023-12-13 21:42:54 +0000

    Update Showcase.js

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -9,11 +9,11 @@ const Showcase = () => {
       <main className="showcase">
         <h1 className="showcase-header h-color">BOT</h1>
         <h2 className="showcase-header-text h-color">
-          The only discord bot you need!
+          The only ER:LC discord bot you need!
         </h2>
         <p className="showcase-para p-color">
-          One of the awesome discord bot with tons of commands that will fill
-          your needs in your discord server.
+          One of the best Discord bots to cover all
+          your needs in your ER:LC Discord server.
         </p>
         <Image
           src={botOnlineStatus}
